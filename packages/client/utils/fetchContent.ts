ff5a61a3c521725248f2d95a4be4d5fd6e67505d
Reza Kalfane <reza.kalfane@gmail.com>
2021-02-26 00:05:57 +0100

    First commit

--- packages/client/utils/fetchContent.ts
+++ packages/client/utils/fetchContent.ts
@@ -1,14 +1,15 @@
 import fetch from 'isomorphic-unfetch';
 import { NextPageContext } from 'next';
+import { contentApi } from '../config/cms.json'
 
 export function fetchContent<T = any>(key: string, context?: NextPageContext): Promise<T> {
-    return fetch(`https://${context.query.contentApi || process.env.contentApi}/content/key/${key}?depth=all&format=inlined`)
+    return fetch(`https://${context.query.contentApi || contentApi || process.env.contentApi}/content/key/${key}?depth=all&format=inlined`)
             .then(resp => resp.json())
             .then(body => body.content);
 }
 
 export function fetchContentById<T = any>(id: string, context?: NextPageContext): Promise<T> {
-    return fetch(`https://${context.query.contentApi  || process.env.contentApi}/content/id/${id}?depth=all&format=inlined`)
+    return fetch(`https://${context.query.contentApi  || contentApi || process.env.contentApi}/content/id/${id}?depth=all&format=inlined`)
             .then(resp => resp.json())
             .then(body => body.content);
 }
